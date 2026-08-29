c1dbdc959ef922a40e6f5675ed38fe3d722df74c
Kadek Sudiartha <sudieartha@gmail.com>
2026-08-29 13:59:50 +0800

    solitude-light: use ashen force_override for popup highlights

--- themes/solitude-light/neovim.lua
+++ themes/solitude-light/neovim.lua
@@ -1,28 +1,21 @@
 return {
   {
     "ficcdaf/ashen.nvim",
-    init = function()
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "*",
-        callback = function()
-          if vim.g.colors_name ~= "ashen" then
-            return
-          end
-          local bg = "#e8ebee"
-          local fg = "#2c3135"
-          local sel = "#d0d7dc"
-          vim.api.nvim_set_hl(0, "Pmenu", { fg = fg, bg = bg })
-          vim.api.nvim_set_hl(0, "PmenuSel", { fg = fg, bg = sel })
-          vim.api.nvim_set_hl(0, "PmenuSbar", { bg = bg })
-          vim.api.nvim_set_hl(0, "PmenuThumb", { bg = sel })
-          vim.api.nvim_set_hl(0, "NormalFloat", { fg = fg, bg = bg })
-          vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#8a9199", bg = bg })
-          vim.api.nvim_set_hl(0, "FloatTitle", { fg = fg, bg = bg })
-          vim.api.nvim_set_hl(0, "NoicePopup", { fg = fg, bg = bg })
-          vim.api.nvim_set_hl(0, "NoicePopupBorder", { fg = "#8a9199", bg = bg })
-        end,
-      })
-    end,
+    opts = {
+      hl = {
+        force_override = {
+          Pmenu = { fg = "#2c3135", bg = "#e8ebee" },
+          PmenuSel = { fg = "#2c3135", bg = "#d0d7dc" },
+          PmenuSbar = { fg = "NONE", bg = "#e8ebee" },
+          PmenuThumb = { fg = "NONE", bg = "#d0d7dc" },
+          NormalFloat = { fg = "#2c3135", bg = "#e8ebee" },
+          FloatBorder = { fg = "#8a9199", bg = "#e8ebee" },
+          FloatTitle = { fg = "#2c3135", bg = "#e8ebee" },
+          NoicePopup = { fg = "#2c3135", bg = "#e8ebee" },
+          NoicePopupBorder = { fg = "#8a9199", bg = "#e8ebee" },
+        },
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
