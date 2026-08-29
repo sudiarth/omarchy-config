3771234263cab0e4d0bd73a36fb06991aedd91ec
Kadek Sudiartha <sudieartha@gmail.com>
2026-08-29 20:09:24 +0800

    solitude-light: use aether with darker popup/dialog bgcolor and high contrast text

--- themes/solitude-light/neovim.lua
+++ themes/solitude-light/neovim.lua
@@ -1,26 +1,77 @@
 return {
   {
-    "ficcdaf/ashen.nvim",
+    "bjarneo/aether.nvim",
+    branch = "v3",
+    name = "aether",
+    priority = 1000,
     opts = {
-      hl = {
-        force_override = {
-          Pmenu = { fg = "#2c3135", bg = "#e8ebee" },
-          PmenuSel = { fg = "#2c3135", bg = "#d0d7dc" },
-          PmenuSbar = { fg = "NONE", bg = "#e8ebee" },
-          PmenuThumb = { fg = "NONE", bg = "#d0d7dc" },
-          NormalFloat = { fg = "#2c3135", bg = "#e8ebee" },
-          FloatBorder = { fg = "#8a9199", bg = "#e8ebee" },
-          FloatTitle = { fg = "#2c3135", bg = "#e8ebee" },
-          NoicePopup = { fg = "#2c3135", bg = "#e8ebee" },
-          NoicePopupBorder = { fg = "#8a9199", bg = "#e8ebee" },
-        },
+      styles = {
+        floats = "dark",
+        sidebars = "dark",
       },
+      colors = {
+        bg = "#e8ebee",
+        dark_bg = "#d0d5db",
+        darker_bg = "#c2c8cf",
+        lighter_bg = "#f0f2f4",
+
+        fg = "#2c3135",
+        dark_fg = "#1a1e21",
+        light_fg = "#4a5055",
+        bright_fg = "#5c6369",
+        muted = "#7a828a",
+
+        red = "#8a6050",
+        yellow = "#7a7560",
+        orange = "#8a6050",
+        green = "#5a7060",
+        cyan = "#607070",
+        blue = "#506878",
+        magenta = "#706070",
+        brown = "#7a7560",
+
+        bright_red = "#b84535",
+        bright_yellow = "#8a7a55",
+        bright_green = "#3a6050",
+        bright_cyan = "#457070",
+        bright_blue = "#3a5868",
+        bright_magenta = "#704060",
+
+        accent = "#5a6370",
+        cursor = "#1a1e21",
+        foreground = "#2c3135",
+        background = "#e8ebee",
+        selection = "#bac2ca",
+        selection_foreground = "#1a1e21",
+        selection_background = "#bac2ca",
+      },
+      on_highlights = function(hl, c)
+        local popup_bg = "#d0d5db"
+        local popup_fg = "#1a1e21"
+        local popup_border = "#8a9199"
+        local sel_bg = "#bac2ca"
+
+        hl.Pmenu = { bg = popup_bg, fg = popup_fg }
+        hl.PmenuSel = { bg = sel_bg, fg = popup_fg, bold = true }
+        hl.PmenuMatch = { bg = popup_bg, fg = c.blue, bold = true }
+        hl.PmenuMatchSel = { bg = sel_bg, fg = c.blue, bold = true }
+        hl.PmenuSbar = { bg = popup_bg }
+        hl.PmenuThumb = { bg = "#8a9199" }
+
+        hl.NormalFloat = { bg = popup_bg, fg = popup_fg }
+        hl.FloatBorder = { bg = popup_bg, fg = popup_border }
+        hl.FloatTitle = { bg = popup_bg, fg = popup_fg, bold = true }
+
+        hl.WhichKeyFloat = { bg = popup_bg, fg = popup_fg }
+        hl.SnacksNormal = { bg = popup_bg, fg = popup_fg }
+        hl.SnacksBorder = { bg = popup_bg, fg = popup_border }
+      end,
     },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "ashen",
+      colorscheme = "aether",
     },
   },
 }
