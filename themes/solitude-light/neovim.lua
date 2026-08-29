46304a4c0c08d656a32cb0cfec3a0595d7101239
Kadek Sudiartha <sudieartha@gmail.com>
2026-08-29 12:40:57 +0800

    solitude-light: match popover menu background to theme background

--- themes/solitude-light/neovim.lua
+++ themes/solitude-light/neovim.lua
@@ -1,9 +1,24 @@
 return {
-  { "ficcdaf/ashen.nvim" },
+  {
+    "ficcdaf/ashen.nvim",
+    after = function()
+      local function override_pmenu()
+        local bg = "#e8ebee"
+        vim.api.nvim_set_hl(0, "Pmenu", { fg = "#2c3135", bg = bg })
+        vim.api.nvim_set_hl(0, "PmenuSel", { fg = "#2c3135", bg = "#d0d7dc" })
+        vim.api.nvim_set_hl(0, "PmenuSbar", { bg = bg })
+        vim.api.nvim_set_hl(0, "PmenuThumb", { bg = "#d0d7dc" })
+      end
+      override_pmenu()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = override_pmenu,
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "ashen",
     },
-  }
+  },
 }
