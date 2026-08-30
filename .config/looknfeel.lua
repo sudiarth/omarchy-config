f3055b5b8a345d6cab67361c506d364c44fbae55
Kadek Sudiartha <sudieartha@gmail.com>
2026-08-30 10:50:03 +0800

    Update Hyprland config: add rounding, fix gaps

---
+++ .config/looknfeel.lua
@@ -0,0 +1,12 @@
+-- Change the default Omarchy look'n'feel.
+
+hl.config({
+  general = {
+    gaps_in = 5,
+    gaps_out = 10,
+    border_size = 5,
+  },
+  decoration = {
+    rounding = 10,
+  },
+})
